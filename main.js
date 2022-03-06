915da20ee7b4452e83f45b7bdc41ae2f3dafe9f3
pronepoet <ochiengphillip84@gmail.com>
2022-03-07 00:02:11 +0300

    adding the function name and the values

--- main.js
+++ main.js
@@ -6,5 +6,9 @@ let year = document.getElementById(year)
 
 
 function get(){
+    let CC = year.slice(0,2);
+    let YY = year.slice(2,4);
+    let MM = month.value;
+    let DD = date.value;
     d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 } 
