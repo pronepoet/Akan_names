e724c51c4ae95adf796d99eeecdb1bb0cc9e1eb9
pronepoet <ochiengphillip84@gmail.com>
2022-03-07 01:29:36 +0300

    preventing default event listener

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 let date = document.getElementById("date")
 let month = document.getElementById("month")
-let year = document.getElementById("year").value;
-let button = document.getElementById("button");
+let year = document.getElementById("year");
+let button = document.getElementById("get_name");
 
 
 
 function get(){
  
-    let CC = year.slice(0,2);
-    let YY = year.slice(2,4);
+    let CC = year.value.slice(0,2);
+    let YY = year.value.substring(2);
     let MM = month.value;
     let DD = date.value;
-    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    console.log(d)
+   let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
+    console.log(d.toFixed())
+    
   button.addEventListener('click', function(e){
-    e.preventDefault();
-    get();
+       e.preventDefault();
+
+       get();
   })
 } 
