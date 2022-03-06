0711e80905963387e73d434e175ed78cb47462c3
pronepoet <ochiengphillip84@gmail.com>
2022-03-06 23:42:11 +0300

    adding the function and declaring variables in js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let CC = document.getElementById(date);
+
+let YY = document.getElementById(year);
+
+let MM = document.getElementById(month)
+
+d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
