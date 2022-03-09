518be9dbc1dafc7630505cb26320cd57cdf9764a
pronepoet <ochiengphillip84@gmail.com>
2022-03-09 10:29:52 +0300

    Ft:adding error handling for when a user enters invalid details which shows an alert

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let date1 = document.getElementById("date")
 let month = document.getElementById("month")
 let year = document.getElementById("year");
 let button = document.getElementById("get_name");
+let display = document.getElementById("waria");
 
 
 
@@ -11,26 +12,35 @@ function calculate() {
 
     let MM = month.value;
     let DD = date1.value;
-    const date = new Date(YY, MM, DD);
+    let date = new Date(YY, MM, DD);
+
+    if (MM <= 0 || MM > 12 || DD <= 0 || DD > 31 || YY.legth > 4 || YY.legth <= 0) {
+        alert("Please enter valid details for the dates");
+        date = "";
+        //let display = document.getElementById("waria");
+        display.innerHTML = "";
+
+    } else {
+        return date.getDay();
+    }
 
 
-
-    return date.getDay();
 
 
 }
 
 function getName() {
-    let display = document.getElementById("waria");
+    // let display = document.getElementById("waria");
     let gender = document.getElementById("genderio")
 
     let index = calculate();
     let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
     let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-    if (gender.value === "Female") {
+
+    if (gender.value === "Female" && index != null) {
         let femName = female[index]
         display.innerHTML = `<h1>Your name is ${femName}</h1>`;
-    } else if (gender.value === "Male") {
+    } else if (gender.value === "Male" && index != null) {
         let maleName = male[index]
         display.innerHTML = `<h1>Your name is ${maleName}</h1>`;
     }
@@ -42,4 +52,5 @@ button.addEventListener('click', function (e) {
     e.preventDefault();
 
     getName();
+
 })
